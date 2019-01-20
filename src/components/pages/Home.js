12d5f6e5339d362c4dd14640c6fba5a96f68b37c
Richard Tzanov <richard@jdlt.co.uk>
2019-01-20 18:34:16 +0000

    create boilerplate

---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="section">
+        <div className="container">
+          <h1 className="title is-1">Petia's site</h1>
+        </div>
+      </div>
+    );
+  }
+}
