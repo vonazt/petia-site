1500652c6d959a6a3a3471d00d69c7202d07a6ff
Richard Tzanov <richard@jdlt.co.uk>
2019-03-21 13:25:35 +0000

    make landing page mob responsive

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,14 +2,14 @@ import React, { Component } from "react";
 export default class Home extends Component {
   render() {
     return (
-      <section class="hero is-fullheight">
-        <div class="hero-body">
-          <div class="container">
-            <h1 class="landing-title gradient">Petia Tzanova</h1>
-            <h2 class="landing-subtitle">Coaching & Facilitation</h2>
-            <h2 class="landing-subtitle">Project Design & Management</h2>
-            <h2 class="landing-subtitle">Consulting</h2>
-            <h3 class="landing-subtitle gradient">
+      <section className="hero is-fullheight">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="landing-title gradient">Petia Tzanova</h1>
+            <h2 className="landing-subtitle">Coaching & Facilitation</h2>
+            <h2 className="landing-subtitle">Project Design & Management</h2>
+            <h2 className="landing-subtitle">Consulting</h2>
+            <h3 className="landing-subtitle gradient">
               Full website coming soon. Meanwhile drop me a note at{" "}
               <a href="mailto:hello@petia.uk" target="_blank" rel="noopener noreferrer" style={{borderBottom: "1px solid white"}}>
                hello@petia.uk
